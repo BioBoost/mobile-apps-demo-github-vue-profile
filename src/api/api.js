81c3cd6ca9b6b391bda5e732091adb7183991719
Nico De Witte <nico.dewitte@outlook.com>
2020-10-08 14:07:44 +0200

    Add authentication github

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,14 @@
 import axios from 'axios';
 
 export default () => {
+  // npm install --save dotenv
+  // console.log(`TOKEN ${process.env.VUE_APP_API_TOKEN}`);
+
   let api = axios.create({
-    baseURL: `http://localhost:3000`
+    baseURL: `https://api.github.com`,
+    headers: {
+      Authorization: `Bearer ${process.env.VUE_APP_API_TOKEN}`
+    }
   });
 
   return api;
