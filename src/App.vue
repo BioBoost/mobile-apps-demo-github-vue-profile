025a5e6fd389886f24aba6c793001ff4c56c30a3
Nico De Witte <nico.dewitte@outlook.com>
2020-10-08 13:00:22 +0200

    Import component

--- src/App.vue
+++ src/App.vue
@@ -11,61 +11,6 @@
       <v-row justify="center">
         <v-col cols="12" md="6" lg="4">
 
-          <v-card class="rounded-t-xl">
-            <v-toolbar color="grey darken-3" class="white--text" flat>
-              <v-spacer></v-spacer>
-              <v-toolbar-title class="font-weight-bold">
-                <h2>{{ user.name }}</h2>
-              </v-toolbar-title>
-              <v-spacer></v-spacer>
-            </v-toolbar>
-
-            <div class="text-center ma-3" v-if="user.company">
-              <span>Works @ {{ user.company }}</span>
-            </div>
-
-            <div class="text-center ma-3">
-              <v-avatar class="ma-3" size="124">
-                <v-img :src="user.avatar_url"></v-img>
-              </v-avatar>
-            </div>
-
-            <v-card-text class="primary lighten-1 text-center white--text">
-
-              <v-row>
-                <v-col cols="12" sm="6">
-                  <span>Public Repos</span>
-                </v-col>
-                <v-col cols="12" sm="6">
-                  <h2>{{ user.public_repos }}</h2>
-                </v-col>
-              </v-row>
-
-              <v-divider />
-
-              <v-row>
-                <v-col cols="12" sm="6">
-                  <span>Followers</span>
-                </v-col>
-                <v-col cols="12" sm="6">
-                  <h2>{{ user.followers }}</h2>
-                </v-col>
-              </v-row>
-
-              <v-divider />
-
-              <v-row>
-                <v-col cols="12" sm="6">
-                  <span>Following</span>
-                </v-col>
-                <v-col cols="12" sm="6">
-                  <h2>{{ user.following }}</h2>
-                </v-col>
-              </v-row>
-
-            </v-card-text>
-
-          </v-card>
 
         </v-col>
       </v-row>
@@ -76,9 +21,11 @@
 
 <script>
 import UsersAPI from './api/users';
+import UserDetails from "@/components/UserDetails";   // @ = src dir
 
 export default {
   name: 'App',
+  
 
   created() {
     // console.log("Loading user ...");
